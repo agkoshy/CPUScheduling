fb96b6b58b57f79f2d98aab9e233546d241ab29c
agkoshy <alviskoshy@gmail.com>
2017-11-08 00:16:42 -0500

    Finished RR for most part, added some methods for Process

--- src/cpuscheduling/Process.java
+++ src/cpuscheduling/Process.java
@@ -1,10 +1,14 @@
 package cpuscheduling;
 
+import java.util.Scanner;
+
 public class Process {
 	
 	int arrivalTime;
 	int reqTime;
 	int priority;
+	int[] burst;
+	Scanner scn = new Scanner(System.in);
 	
 	/*
 	private enum state
@@ -23,6 +27,26 @@ public class Process {
 		this.arrivalTime = arrivalTime;
 		this.reqTime = reqTime;
 		this.priority = priority;
+		burst = new int[this.numOfProcesses()];
+	}
+	
+	public int numOfProcesses() {
+		System.out.println("Enter number of Processes");
+		return scn.nextInt();
+	} 
+	
+	public int[] burstTime() {
+		int i;
+		System.out.println("Enter burst time");
+		for(i = 0; i < this.numOfProcesses(); i++) {
+			burst[i] = scn.nextInt();
+		}
+		return burst;
+	}
+	
+	public int quantumTime() {
+		System.out.println("Enter quantum time");
+		return scn.nextInt();
 	}
 
 }
--- src/cpuscheduling/Schedule.java
+++ src/cpuscheduling/Schedule.java
@@ -1,9 +1,16 @@
 package cpuscheduling;
 
 public class Schedule {
-
+	
+	Process process;
+	int size, quantum;
+	int[] burst, wait, turnaround;
 	public Schedule()
 	{
+		process = new Process();
+		this.size = process.numOfProcesses();
+		this.burst = process.burstTime();
+		this.quantum = process.quantumTime();
 		
 	}
 	
@@ -62,9 +69,55 @@ public class Schedule {
 	
 	private void P_RR()
 	{
+		int flag;
+		int bt = 0;
+		int ta = 0;
+		int[] prc = new int[size];
+		do{
+			flag=0;
+			for(int i=0; i<size; i++)
+			{
+				if(prc[i]>=quantum)
+				{
+					System.out.print("P"+(i+1));
+					for(int j=0; j<size; j++)
+					{
+						if(j==i)
+							prc[i]=prc[i]-quantum;
+						else if(prc[j]>0)
+							wait[j]+=quantum;
+					}
+				}
+				else if(prc[i]>0)
+				{
+					System.out.print("P"+(i+1));
+					for(int j=0;j<size;j++)
+					{
+						if(j==i)
+							prc[i]=0;
+						else if(prc[j]>0)
+							wait[j]+=prc[i];
+					}
+				}
+			}
+			for(int i=0;i<size;i++)
+				if(prc[i]>0)
+					flag=1;
+		}while(flag==1);
+		for(int i=0;i<size;i++) {
+			turnaround[i]=wait[i]+burst[i];
+		}
 		
+		for(int i = 0; i < size; i++)
+		{
+			bt = bt + wait[i];
+			ta  = ta + turnaround[i];
+		}
+		
+		System.out.println("Waiting time: " + bt/size);
+		System.out.println("Turnaround time: " + ta/size);
 	}
-	
+
 	/*
 	 * Multilevel Queue Scheduling
 	 */
